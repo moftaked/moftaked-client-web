04831f3039183886bb0d1c01491c7ebe9d089f4c
tony-div <tg764562@gmail.com>
2024-09-23 08:39:06 +0300

    feat(home): decoupled class cad & functioning

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AppHeaderComponent } from "../app-header/app-header.component";
-import { Class, classResultBody, UserService } from '../services/user.service';
+import { Class, userClassesResultBody, UserService } from '../services/user.service';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { ClassCardComponent } from '../class-card/class-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AppHeaderComponent],
+  imports: [
+    AppHeaderComponent,
+    ClassCardComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -17,8 +21,8 @@ export class HomeComponent implements OnInit{
   constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
-    const observable = this.userService.getClasses().subscribe({
-      next: (res: HttpResponse<classResultBody>) => {
+    this.userService.getClasses().subscribe({
+      next: (res: HttpResponse<userClassesResultBody>) => {
         if(res.body)
           this.classes = res.body.results;
       },
@@ -31,4 +35,7 @@ export class HomeComponent implements OnInit{
     })
   }
 
+  onCardClick(classId: string) {
+    this.router.navigate([`/class/${classId}`]);
+  }
 }
